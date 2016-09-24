31ab509af16c670c47b9ba1db57ccdc373cf40c1
Jae Gomez <emailjuliogomez@gmail.com>
2016-09-24 14:55:28 -0400

    added new file with dependencies etc

---
+++ BamazonCustomer.js
@@ -0,0 +1,8 @@
+// dependencies
+var mysql = require('mysql');
+var prompt = require('prompt');
+var inquirer = require('inquirer');
+
+//starting connection to my database below
+
+var connectionCommence = mysql.
