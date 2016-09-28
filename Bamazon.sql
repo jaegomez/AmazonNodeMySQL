3457fe12734284b2dd52ee016845ea057f1eaa7b
Jae Gomez <emailjuliogomez@gmail.com>
2016-09-28 17:05:38 -0400

    functioning app, added remainder of js code to acces mysql and prompt messages etc

--- Bamazon.sql
+++ Bamazon.sql
@@ -5,9 +5,9 @@ create database Bamazon;
 use Bamazon;
 
 create table 'Products' (
-  `itemId` INT NOT NULL AUTO_INCREMENT,
-  `productName` VARCHAR(45) NULL,
-  'departmentName' VARCHAR(45) null,
+  `itemID` INT NOT NULL AUTO_INCREMENT,
+  `productName` VARCHAR(99) NULL,
+  'departmentName' VARCHAR(99) NULL,
   `price` DECIMAL(10,2) NULL,
   `stockQuantity` INT NULL,
   PRIMARY KEY (`id`)
